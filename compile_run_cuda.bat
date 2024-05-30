1bf83763b3081fd4e92448a7dcd93ef0d2c7e08f
xFrah <fdimonaco309@hotmail.com>
2024-05-30 12:29:16 +0200

    Better cuda .bat file

--- compile_run_cuda.bat
+++ compile_run_cuda.bat
@@ -1,1 +1,3 @@
-nvcc main_cuda.cu common.c libs/cJSON.c libs/win_socket_server.c -o main_cuda && main_cuda.exe
+nvcc main_cuda.cu common.c libs/cJSON.c libs/win_socket_server.c -o main_cuda
+del main_cuda.exp main_cuda.lib
+main_cuda.exe
